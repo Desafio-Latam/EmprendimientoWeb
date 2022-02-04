b4fe01740e9f1e63bc93f7d75befc6dd2e61ed7c
kofrann <kofran.cl@gmail.com>
2022-02-04 17:43:17 -0300

    Se sube prueba emprendimiento web, falta animacion y que sea mas responsiva, falta agregar grid. y resolver problemas en  las columnas.  se sigue aprendiendo.

---
+++ assets/js/style.js
@@ -0,0 +1,17 @@
+
+const texto1 = "Hola"
+const texto2 = "Desafio"
+const texto3 = "Latam";
+const alumno1 = "Franco Urra";
+const alumno2 = "Josefina Urra";
+const alumno3 = "Federico Urra";
+
+
+constsaludoInterpolado = `${texto1} ${texto2} ${texto3}`;
+  const alumnos = `Los Alumnos de Front End G:19 son: 
+                    ${alumno1}, ${alumno2}, ${alumno3}`;
+console.log(alumnos);
+
+
+
+
